64874a22a203ddc0e14b26a352c3cb8c1438622a
edsonajeje <edsonero@hotmail.com>
2019-04-16 10:48:11 -0300

    Update Buscas.h

--- Buscas.h
+++ Buscas.h
@@ -2,29 +2,29 @@
 #ifndef Buscas
 #define Buscas
 
-bool sequencial (long int a[], int chave, int n);
-bool binaria (long int a[], int chave, int n);
+bool sequencial (User a[], int chave, int n);
+bool binaria (User a[], int chave, int n);
 
 #endif
 
-bool sequencial(long int a[], int chave, int n){
+bool sequencial(User a[], int chave, int n){
 	int pos=0;
 	bool encontrado=false;
-	while(a[pos]!=chave&&pos<n)
+	while(a[pos].cod!=chave&&pos<n)
 		pos++;
 	if(pos<n)
 		encontrado=true;
 	return encontrado;
 }
 
-bool binaria(long int a[], int chave, int n){
+bool binaria(User a[], int chave, int n){
 	int meio, dir=n-1, esq=0;
 	bool encontrado=false;
 	while(esq<=dir&&encontrado==false){
 		meio=(esq+dir)/2;
-		if(a[meio]==chave){
+		if(a[meio].cod==chave){
 			encontrado=true;
-		}else if(chave<a[meio]){
+		}else if(chave<a[meio].cod){
 			dir=meio-1;
 		}else{
 			esq=meio+1;
