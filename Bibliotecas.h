32dceb25d94d79c99069ab9f79c615bb567f7615
edsonajeje <edsonero@hotmail.com>
2019-04-16 10:47:37 -0300

    Create Bibliotecas.h

---
+++ Bibliotecas.h
@@ -0,0 +1,21 @@
+
+#ifndef Bibliotecas
+#define Bibliotecas
+
+#include "BubbleSort.h"
+#include "InsertionSort.h"
+#include "SelectionSort.h"
+#include "ShellSort.h"
+#include "MergeSort.h"
+#include "QuickSort.h"
+
+#include "Buscas.h"
+#include "CompararBuscas.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/time.h>
+#include <string.h>
+
+#endif
