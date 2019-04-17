93a4da6d2b28e83583d730cc85280c0b86aac36c
edsonajeje <edsonero@hotmail.com>
2019-04-17 11:51:37 -0300

    Update SelectionSort.h

--- SelectionSort.h
+++ SelectionSort.h
@@ -2,20 +2,21 @@
 #ifndef SelectionSort
 #define SelectionSort
 
-void selection(int a[], int n);
+void selection(User a[], int n);
 
 
 #endif
 
-void selection(int a[], int n){
-	int idChave, menorChave, idMenor, idTemp, aux;
+void selection(User a[], int n){
+	int idChave, menorChave, idMenor, idTemp;
+	User aux;
 	
 	for(idChave=0; idChave<=n-2; idChave++){
-		menorChave=a[idChave];
+		menorChave=a[idChave].cod;
 		idMenor=idChave;
 		for(idTemp=idChave+1;idTemp<=n-1;idTemp++){
-			if(a[idTemp]<menorChave){
-				menorChave=a[idTemp];
+			if(a[idTemp].cod<menorChave){
+				menorChave=a[idTemp].cod;
 				idMenor=idTemp;
 			}
 		}
