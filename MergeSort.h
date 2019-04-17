0bf035a1c71bd49425d924e856bd2f6a1a44cc77
edsonajeje <edsonero@hotmail.com>
2019-04-17 11:51:27 -0300

    Update MergeSort.h

--- MergeSort.h
+++ MergeSort.h
@@ -2,18 +2,18 @@
 #ifndef MergeSort
 #define MergeSort
 
-void merge(int a[], int p, int q, int r);
-void mergeSort(int a[], int p, int r);
+void merge(User a[], int p, int q, int r);
+void mergeSort(User a[], int p, int r);
 
 
 #endif
 
-void merge(int a[], int p, int q, int r){
+void merge(User a[], int p, int q, int r){
 	int n1, n2, i, j, k;
 	n1=q-p+1;
 	n2=r-q;
 	
-	int e[n1+1], d[n2+1];
+	User *e=(User*)malloc((n1+1)*sizeof(User)); User *d=(User*)malloc(sizeof(User)*(n2+1));
 	
 	for(i=0;i<n1;i++){
 		e[i]=a[p+i];
@@ -23,11 +23,11 @@ void merge(int a[], int p, int q, int r){
 		d[j]=a[q+1+j];
 	}
 	
-	e[n1]=d[n2]=999999999;
+	e[n1].cod=d[n2].cod=999999999;
 	i=j=0;
 	
 	for(k=p;k<=r;k++){
-		if(e[i]<=d[j]){
+		if(e[i].cod<=d[j].cod){
 			a[k]=e[i];
 			i++;
 		}else{
@@ -37,7 +37,7 @@ void merge(int a[], int p, int q, int r){
 	}
 }
 
-void mergeSort(int a[], int p, int r){
+void mergeSort(User a[], int p, int r){
 	int q;
 	if(p<r){
 		q=(p+r)/2;
