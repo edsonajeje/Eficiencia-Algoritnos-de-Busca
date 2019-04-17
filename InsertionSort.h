12e8216c50265a6a285ddd284f8cb0e2cac85fcb
edsonajeje <edsonero@hotmail.com>
2019-04-17 11:51:07 -0300

    Update InsertionSort.h

--- InsertionSort.h
+++ InsertionSort.h
@@ -2,18 +2,19 @@
 #ifndef InsertionSort
 #define InsertionSort
 
-void insertion(int a[], int n);
+void insertion(User a[], int n);
 
 
 #endif
 
-void insertion(int a[], int n){
-	int chaveAtual, idChave, i;
+void insertion(User a[], int n){
+	int idChave, i;
+	User chaveAtual;
 	
 	for(idChave=0; idChave<=n-1; idChave++){
 		chaveAtual=a[idChave];
 		i=idChave-1;
-		while(i>=0&&a[i]>chaveAtual){
+		while(i>=0&&a[i].cod>chaveAtual.cod){
 			a[i+1]=a[i];
 			i--;
 		}
