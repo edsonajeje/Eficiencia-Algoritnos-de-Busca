6e8ca52f381954a10070243ac6c38fce26bed00c
edsonajeje <edsonero@hotmail.com>
2019-04-22 03:41:55 -0300

    Update Bibliotecas.h

--- Bibliotecas.h
+++ Bibliotecas.h
@@ -8,14 +8,16 @@
 #include "ShellSort.h"
 #include "MergeSort.h"
 #include "QuickSort.h"
+#include "HeapSort.h"
 
 #include "Buscas.h"
 #include "CompararBuscas.h"
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-#include <sys/time.h>
+//#include <time.h>
+//#include <sys/time.h>
+#include <ctime>
 #include <string.h>
 
 #endif
