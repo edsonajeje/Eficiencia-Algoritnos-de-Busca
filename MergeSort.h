fef74579d82ad2a56618574cea5bbe0394097869
edsonajeje <edsonero@hotmail.com>
2019-04-22 03:43:14 -0300

    Update MergeSort.h

--- MergeSort.h
+++ MergeSort.h
@@ -16,11 +16,17 @@ void merge(User a[], int p, int q, int r){
 	User *e=(User*)malloc((n1+1)*sizeof(User)); User *d=(User*)malloc(sizeof(User)*(n2+1));
 	
 	for(i=0;i<n1;i++){
-		e[i]=a[p+i];
+		e[i].cod=a[p+i].cod;
+		strcpy(e[i].email,a[p+i].email);
+		strcpy(e[i].nome,a[p+i].nome);
+		strcpy(e[i].senha,a[p+i].senha);
 	}
 	
 	for(j=0;j<n2;j++){
-		d[j]=a[q+1+j];
+		d[j].cod=a[q+1+j].cod;
+		strcpy(d[j].email,a[q+1+j].email);
+		strcpy(d[j].nome,a[q+1+j].nome);
+		strcpy(d[j].senha,a[q+1+j].senha);
 	}
 	
 	e[n1].cod=d[n2].cod=999999999;
@@ -28,10 +34,16 @@ void merge(User a[], int p, int q, int r){
 	
 	for(k=p;k<=r;k++){
 		if(e[i].cod<=d[j].cod){
-			a[k]=e[i];
+			a[k].cod=e[i].cod;
+			strcpy(a[k].email,e[j].email);
+			strcpy(a[k].nome,e[j].nome);
+			strcpy(a[k].senha,e[j].senha);
 			i++;
 		}else{
-			a[k]=d[j];
+			a[k].cod=d[j].cod;
+			strcpy(a[k].email,d[j].email);
+			strcpy(a[k].nome,d[j].nome);
+			strcpy(a[k].senha,d[j].senha);
 			j++;
 		}
 	}
