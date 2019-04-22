a59788d6553b13911b4d24c33fd1e751c36fdd59
edsonajeje <edsonero@hotmail.com>
2019-04-22 15:24:37 -0300

    Update FormatarSaida.h

--- FormatarSaida.h
+++ FormatarSaida.h
@@ -1,8 +1,17 @@
-#include "Interfaces.h"
 
 #ifndef FormatarSaida
 #define FormatarSaida
 
+int verificaSequencia(FILE *a, int ord);
 
 
 #endif
+
+
+int verificaSequencia(FILE *a, int ord){
+	a=fopen("sequencia.txt","w");
+	if(a!=NULL)
+		fprintf(a,"%d\t",ord);
+	else
+		printf("\nErro ao abrir o arquivo");
+}
