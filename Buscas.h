f47bda41d8f3d0f5f554d27033d26312c7a82aec
edsonajeje <edsonero@hotmail.com>
2019-04-22 03:42:05 -0300

    Update Buscas.h

--- Buscas.h
+++ Buscas.h
@@ -2,34 +2,42 @@
 #ifndef Buscas
 #define Buscas
 
-bool sequencial (User a[], int chave, int n);
-bool binaria (User a[], int chave, int n);
+bool sequencial (User a[], int cont, int chave[], int n);
+bool binaria (User a[], int cont, int chave[], int n);
 
 #endif
 
-bool sequencial(User a[], int chave, int n){
+bool sequencial(User a[], int cont, int chave[], int n){
 	int pos=0;
 	bool encontrado=false;
-	while(a[pos].cod!=chave&&pos<n){
+	while(a[pos].cod!=chave[cont]&&pos<n){
+		//printf("entrou no while do sequencial vez %d\t",pos);
 		pos++;
 	}
 	if(pos<n)
 		encontrado=true;
+	/*if(encontrado)
+		printf("Encontrado\t");*/
 	return encontrado;
+	
 }
 
-bool binaria(User a[], int chave, int n){
-	int meio, dir=n-1, esq=0;
+bool binaria(User a[], int cont , int chave[], int n){
+	int meio, dir=n-1, esq=0, x=0;
 	bool encontrado=false;
 	while(esq<=dir&&encontrado==false){
+		//printf("entrou no while do binaria\t",x);
 		meio=(esq+dir)/2;
-		if(a[meio].cod==chave){
+		if(a[meio].cod==chave[cont]){
 			encontrado=true;
-		}else if(chave<a[meio].cod){
+		}else if(chave[cont]<a[meio].cod){
 			dir=meio-1;
 		}else{
 			esq=meio+1;
 		}
+		x++;
 	}
+	/*if(encontrado)
+		printf("Encontrado\t");*/
 	return encontrado;
 }
